397181dd32b9661b343e0760761e351da653dbab
Daniel Ramos de Oliveira Tenório Bomfim <daniel-danitenorio@hotmail.com>
2024-12-13 10:26:52 -0300

    Inicio do banco de dados
    A tabela Usuário foi criada

--- Projeto/SAFERapp/beans/Enums.py
+++ Projeto/SAFERapp/beans/Enums.py
@@ -1,23 +1,23 @@
 from enum import Enum
 
 class RelacaoUFRPE(Enum):
-    docente = 1
-    discente = 2
-    tecnico = 3
-    terceirizado = 4
-    visitante = 5
+    docente = 1, "Docente"
+    discente = 2, "Discente"
+    tecnico = 3, "Técnico"
+    terceirizado = 4, "Terceirizado"
+    visitante = 5, "Visitante"
 
 class TipoUsuario(Enum):
-    admin = 1
-    gestor = 2
-    analista = 2
-    comum = 3
+    admin = 1, "Admin"
+    gestor = 2, "Gestor"
+    analista = 3, "Analista"
+    comum = 4, "Comum"
 
 class Registro(Enum):
-    presenca = 1
-    intervencao = 2
+    presenca = 1, "Presença"
+    intervencao = 2, "Intervenção"
 
 class Status(Enum):
-    aberto = 1
-    emAnalise = 2
-    fechado = 3
+    aberto = 1, "Aberto"
+    emAnalise = 2, "Em andamento"
+    fechado = 3, "Fechado"
