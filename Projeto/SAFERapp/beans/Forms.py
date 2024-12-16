d2c5fdea9861852bd4251cf4a7e136a3b68e5f77
Daniel Ramos de Oliveira Tenório Bomfim <daniel-danitenorio@hotmail.com>
2024-12-15 23:23:55 -0300

    Formulario carrega

--- Projeto/SAFERapp/beans/Forms.py
+++ Projeto/SAFERapp/beans/Forms.py
@@ -3,7 +3,7 @@ from SAFERapp.beans.Ocorrencia import Ocorrencia
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
-class FormularioForm(forms.Form):
+class FormularioForm(forms.ModelForm):
     class Meta:
         model = Ocorrencia
         fields = ['Nome_Autor', 'Celular_Autor', 'Telefone_Autor', 'Relacao_Autor', 'Nome_Animal', 'Local', 'Referencia', 'Tipo_Caso', 'Descricao']
@@ -16,6 +16,6 @@ class FormularioForm(forms.Form):
         self.helper = FormHelper(self)
         self.helper.form_method = 'POST'
         self.helper.form_class = 'form-horizontal'
-        self.helper.form_tag = False  # Não renderizar a tag <form> automaticamente
+        self.helper.form_tag = True  # Não renderizar a tag <form> automaticamente
 
         self.helper.add_input(Submit('submit', 'Enviar'))
