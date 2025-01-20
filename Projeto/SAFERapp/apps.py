6ef99b685aab4febb414245d54ca6feb7faef7ca
Samuel Roberto <developer.snowstorm590@passinbox.com>
2025-01-20 15:55:53 -0300

    Importação do signals

--- Projeto/SAFERapp/apps.py
+++ Projeto/SAFERapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SaferappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'SAFERapp'
+
+    def ready(self):
+        import SAFERapp.signals # importa os signals
