21dcada998cce76cccf34677eaca3acd4f330c97
Samuel Roberto <developer.snowstorm590@passinbox.com>
2025-02-06 11:43:38 -0300

    Sinal para enviar email ao alterar status do chamado adicionado

--- Projeto/SAFERapp/signals.py
+++ Projeto/SAFERapp/signals.py
@@ -3,6 +3,8 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from SAFERapp.models import CustomUser  # Ajuste conforme o nome do seu app
+from SAFERapp.beans.Ocorrencia import Ocorrencia 
+
 
 @receiver(post_save, sender=CustomUser)
 def enviar_email_boas_vindas(sender, instance, created, **kwargs):
@@ -13,3 +15,14 @@ def enviar_email_boas_vindas(sender, instance, created, **kwargs):
         recipient_list = [instance.email]
 
         send_mail(subject, message, from_email, recipient_list)
+
+
+
+@receiver(post_save, sender=Ocorrencia)
+def envia_email_status_alterado(sender, instance, **kwargs):
+    if instance.pk: # verifica se já existe no banco (meio redudante)
+        subject = 'Status da Ocorrência!' # Titulo do email
+        message = f'Olá {instance.Nome_Autor}, status da ocorrência do animal {instance.Nome_Animal} foi alterado para {instance.Status}!' # Corpo do email
+        from_email = settings.DEFAULT_FROM_EMAIL  # Usando o valor configurado no settings.py
+        recipient_list = [instance.Autor.email] # Lista de destinatários
+        send_mail(subject, message, from_email, recipient_list) # Envia o email
