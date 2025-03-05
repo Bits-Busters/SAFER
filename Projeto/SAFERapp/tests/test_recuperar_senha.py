2709b98ac801d209336385f31d9fa02286a99c30
Samuel Roberto <developer.snowstorm590@passinbox.com>
2025-03-05 16:09:20 -0300

    Testes de recuperar senha para outros tipos

--- Projeto/SAFERapp/tests/test_recuperar_senha.py
+++ Projeto/SAFERapp/tests/test_recuperar_senha.py
@@ -42,26 +42,87 @@ class TestInterface(LiveServerTestCase):
         # Verifica se foi redirecionado para pagina de email enviado
         self.assertIn("senha-reset-feito", self.driver.current_url)
 
-
-
+"""
+Realiza testes no backend da aplicação
+"""
 class TestBackend(TestCase):
-    """ 
-    Faz testes de requisições de envio de email da redefinição de senha
-    """
+   
     def setUp(self):
         """Cria um usuário de teste para verificar a redefinição de senha"""
         self.user = CustomUser.objects.create_user(
             nome="testuser", email="testuser@example.com", password="testpassword"
         )
-        mail.outbox = []
-    def test_enviar_email_redefinicao_senha(self):
+        mail.outbox = [] 
         
+        """ 
+        Faz testes de requisições de envio de email da redefinição de senha
+        """
+    def test_enviar_email_redefinicao_senha(self):
+
+        """
+        Faz teste para usuário comum
+        """
+        self.user.tipo_usuario = "Comum"
         # Envia a solicitação de redefinição de senha
-        http_response = self.client.post(reverse('password_reset'), {'email': self.user.email})
+        self.client.post(reverse('password_reset'), {'email': self.user.email})
         # Verifique se o e-mail foi enviado
         self.assertEqual(len(mail.outbox), 1)  # Verifica se um e-mail foi enviado
 
         # Confirma o destinatário
         self.assertEqual(mail.outbox[0].to, [self.user.email])
-
+        mail.outbox = [] #zera caixa de email
         
+        
+        """
+        Faz teste para analista
+        """
+        self.user.tipo_usuario = "Analista"
+        # Envia a solicitação de redefinição de senha
+        self.client.post(reverse('password_reset'), {'email': self.user.email})
+        # Verifique se o e-mail foi enviado
+        self.assertEqual(len(mail.outbox), 1)  # Verifica se um e-mail foi enviado
+
+        # Confirma o destinatário
+        self.assertEqual(mail.outbox[0].to, [self.user.email])
+        mail.outbox = [] #zera caixa de email
+        
+        
+        """
+        Faz teste para gestor
+        """
+        self.user.tipo_usuario = "Gestor"
+        # Envia a solicitação de redefinição de senha
+        self.client.post(reverse('password_reset'), {'email': self.user.email})
+        # Verifique se o e-mail foi enviado
+        self.assertEqual(len(mail.outbox), 1)  # Verifica se um e-mail foi enviado
+
+        # Confirma o destinatário
+        self.assertEqual(mail.outbox[0].to, [self.user.email])
+        mail.outbox = []#zera caixa de email
+        
+        
+        """
+        Faz teste para administrador
+        """
+        self.user.tipo_usuario = "Administrador"
+        # Envia a solicitação de redefinição de senha
+        self.client.post(reverse('password_reset'), {'email': self.user.email})
+        # Verifique se o e-mail foi enviado
+        self.assertEqual(len(mail.outbox), 1)  # Verifica se um e-mail foi enviado
+
+        # Confirma o destinatário
+        self.assertEqual(mail.outbox[0].to, [self.user.email])
+        
+
+    def test_redefinir_senha_comum(self):
+        self.user.tipo_usuario = "Comum"
+        self.user.password = "testpassword"
+
+    def test_redefinir_senha_analista(self):
+        pass
+    
+    def test_redefinir_senha_gestor(self):
+        pass
+
+    def test_redefinir_senha_administrador(self):
+        pass
