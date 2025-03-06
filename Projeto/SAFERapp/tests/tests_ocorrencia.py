c3d9e154cdc5eeed0f90483edaf638a459be042e
Mariane Souza <mariane.elisa@ufrpe.br>
2025-03-06 18:18:37 -0300

    Correções nos testes de visualização de ocorrência e informativos

--- Projeto/SAFERapp/tests/tests_ocorrencia.py
+++ Projeto/SAFERapp/tests/tests_ocorrencia.py
@@ -33,54 +33,64 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-
-
-
-driver = webdriver.Chrome()
-
+from django.test import Client
+from django.contrib.auth import authenticate, login
 
 # Create your tests here.
 
 
 class TesteOcorrencias(StaticLiveServerTestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.usuario_anomino = get_or_create_anonymous_user();
-
-
+    
     def setUp(self):
+        self.usuario_anonimo = CustomUser.objects.create_user(
+                nome="Anônimo Usuário",
+                email='anonimo@example.com',
+                password="senha1234"
+            )
+
         # Inicializa o navegador
         self.driver = webdriver.Chrome()
         self.vars = {}
-        self.live_server_url = "http://localhost:8000" # URL do servidor local
         self.login_user_anonimo()
+
    
     def tearDown(self):
         self.driver.quit()
    
+
     def login_user_anonimo(self):
         # Step # | name | target | value
         # 1 | open | / |
-        self.driver.get("http://localhost:8000/")
+        self.driver.get(self.live_server_url)
         # 2 | setWindowSize | 1235x816 |
         self.driver.set_window_size(1235, 816)
         # 3 | click | id=login |
         self.driver.find_element(By.ID, "login").click()
         # 4 | type | id=login |
-        self.driver.find_element(By.ID, "login").send_keys("anonimo@usuario.com")
+        self.driver.find_element(By.ID, "login").send_keys("anonimo@example.com")
         # 5 | click | id=password |
         self.driver.find_element(By.ID, "password").click()
         # 6 | type | id=password |
         self.driver.find_element(By.ID, "password").send_keys("senha1234")
-   
+
+         # Submetendo o formulário
+        self.driver.find_element(By.ID, "password").send_keys(Keys.RETURN)
+        time.sleep(1)
+    
+    def wait_for_window(self, timeout = 2):
+        time.sleep(round(timeout / 1000))
+        wh_now = self.driver.window_handles
+        if len(wh_now) > len(self.vars["window_handles"]):
+            return set(wh_now).difference(set(self.vars["window_handles"])).pop()
+        return None
+
     """
     Inicio de Testes de visualização de ocorrências
     """
     def criar_ocorrencia_teste(self):
         self.ocorrencia = Ocorrencia.objects.create(
-            Autor=self.usuario,
-            Nome_Autor=self.usuario_anomino.nome,
+            Autor=self.usuario_anonimo,
+            Nome_Autor=self.usuario_anonimo.nome,
             Celular_Autor="000000000000",
             Telefone_Autor="0000000000",
             Relacao_Autor="VISITANTE",
@@ -97,15 +107,10 @@ class TesteOcorrencias(StaticLiveServerTestCase):
         self.ocorrencia.alterar_descricao("Descrição atualizada")
    
     def test_visualizar_ocorrencias_inexistente(self):
-        self.driver.get(self.live_server_url)
-        self.driver.set_window_size(1235, 816)
-        self.driver.find_element(By.CSS_SELECTOR, "#headerRight #hrButton > .btnLoggin").click()
         self.driver.find_element(By.LINK_TEXT, "Meus Chamados").click()
    
     def test_visualizar_ocorrenciar_existentes(self):
         self.criar_ocorrencia_teste()
-        self.driver.get(self.live_server_url)
-        self.driver.set_window_size(1235, 816)
         self.driver.find_element(By.LINK_TEXT, "Meus Chamados").click()
         self.vars["window_handles"] = self.driver.window_handles
         self.driver.find_element(By.CSS_SELECTOR, ".list-group-item > button").click()
@@ -116,8 +121,6 @@ class TesteOcorrencias(StaticLiveServerTestCase):
     def test_visualizar_ocorrencia_atualizada(self):
         self.criar_ocorrencia_teste()
         self.atualizar_ocorrencia_teste()
-        self.driver.get(self.live_server_url)
-        self.driver.set_window_size(1235, 816)
         self.driver.find_element(By.LINK_TEXT, "Meus Chamados").click()
         self.vars["window_handles"] = self.driver.window_handles
         self.driver.find_element(By.CSS_SELECTOR, ".list-group-item > button").click()
@@ -126,5 +129,3 @@ class TesteOcorrencias(StaticLiveServerTestCase):
     """
     Fim de Testes de visualização de ocorrências
     """
-
-
